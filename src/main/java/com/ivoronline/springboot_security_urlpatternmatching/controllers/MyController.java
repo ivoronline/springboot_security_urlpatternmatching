f816b2865b791440b983fefd3fe5a90078191545
igoreta <ivoronline@gmail.com>
2021-01-27 20:29:26 +0100

    m

--- src/main/java/com/ivoronline/springboot_security_urlpatternmatching/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_urlpatternmatching/controllers/MyController.java
@@ -8,25 +8,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MyController {
 
-  @ResponseBody
-  @RequestMapping("/hello")
-  public String sayHello() { return "hello"; }
+  @ResponseBody @RequestMapping("/hello")              public String sayHello() { return "hello";     }
+  @ResponseBody @RequestMapping("/endPoint1")          public String ep1()      { return "endPoint1"; }
+  @ResponseBody @RequestMapping("/endPoint2")          public String ep2()      { return "endPoint2"; }
+  @ResponseBody @RequestMapping("/endPoint3")          public String ep3()      { return "endPoint3"; }
+  @ResponseBody @RequestMapping("/endPoint4")          public String ep4()      { return "endPoint4"; }
+  @ResponseBody @RequestMapping("/sublevel/endPoint5") public String ep5()      { return "endPoint5"; }
 
-  @ResponseBody
-  @RequestMapping("/endPoint1")
-  public String ep1() { return "endPoint1"; }
-
-  @ResponseBody
-  @RequestMapping("/endPoint2")
-  public String ep2() { return "endPoint2"; }
-
-  @ResponseBody
-  @RequestMapping("/endPoint3")
-  public String ep3() { return "endPoint3"; }
-
-
-  @ResponseBody
-  @RequestMapping("/sublevel/endPoint4")
-  public String ep4() { return "endPoint4"; }
+  @GetMapping("/Hello")
+  public String hello() {
+    return "Test";
+  }
 
 }
+
