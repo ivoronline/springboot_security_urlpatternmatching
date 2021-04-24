7f122661c7e5523298304f2d956020a5974caba6
igoreta <ivoronline@gmail.com>
2021-04-24 12:18:20 +0200

    m

---
+++ src/main/java/com/ivoronline/springboot_security_urlpatternmatching/config/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.ivoronline.springboot_security_urlpatternmatching.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  //=================================================================
+  // CONFIGURE (Auth...)
+  //=================================================================
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("myuser" ).password("myuserpassword" ).roles("USER" );
+    auth.inMemoryAuthentication().withUser("myadmin").password("myadminpassword").roles("ADMIN");
+  }
+
+  //=======================================================================
+  // PASSWORD ENCODER
+  //=======================================================================
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+  //=================================================================
+  // CONFIGURE
+  //=================================================================
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+    //SELECT SPECIFIC ENDPOINTS
+    httpSecurity.authorizeRequests()
+      .antMatchers("/endPoint1")              .hasRole("USER")  //Select single  Endpoint
+      .antMatchers("/endPoint2", "/endPoint3").hasRole("USER"); //Select multiple Endpoints from the list
+
+    //SELECT ENDPOINTS - USING ASTERISK '*'
+    httpSecurity.authorizeRequests()
+      .antMatchers("/*"                  ).hasRole("USER")  //Select Endpoints-this level
+      .antMatchers("/cars/*"             ).hasRole("USER")  //Select Endpoints-this level
+      .antMatchers("/cars/end*"          ).hasRole("USER")  //Select Endpoints-this level-start with /end
+      .antMatchers("/cars/*", "/planes*" ).hasRole("USER"); //Select Endpoints that match listed URL Patterns
+
+    //SELECT ENDPOINTS - USING DOUBLE ASTERISK '**'
+    httpSecurity.authorizeRequests()
+      .antMatchers("/**"                 ).hasRole("USER")  //Select Endpoints-any level
+      .antMatchers("/end**"              ).hasRole("USER")  //Select Endpoints-any level-start with /end
+      .antMatchers("/cars/**"            ).hasRole("USER")  //Select Endpoints-any level-start with /cars/
+      .antMatchers("/cars/end**"         ).hasRole("USER")  //Select Endpoints-any level-start with this
+      .antMatchers("/cars/*", "/bikes/**").hasRole("USER"); //Select Endpoints that match listed URL Patterns
+
+    //AUTHENTICATE WITH DEFAULT LOGIN FORM
+    httpSecurity.formLogin(); //Without this there is no Login Form to Authenticate
+
+  }
+
+}
--- src/main/java/com/ivoronline/springboot_security_urlpatternmatching/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_urlpatternmatching/controllers/MyController.java
@@ -1,24 +1,14 @@
 package com.ivoronline.springboot_security_urlpatternmatching.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
-
-  @ResponseBody @RequestMapping("/hello")              public String sayHello() { return "hello";     }
-  @ResponseBody @RequestMapping("/endPoint1")          public String ep1()      { return "endPoint1"; }
-  @ResponseBody @RequestMapping("/endPoint2")          public String ep2()      { return "endPoint2"; }
-  @ResponseBody @RequestMapping("/endPoint3")          public String ep3()      { return "endPoint3"; }
-  @ResponseBody @RequestMapping("/endPoint4")          public String ep4()      { return "endPoint4"; }
-  @ResponseBody @RequestMapping("/sublevel/endPoint5") public String ep5()      { return "endPoint5"; }
-
-  @RequestMapping("/Hello")
-  public String hello() {
-    return "Test";
-  }
-
+  @RequestMapping("/endPoint1")        public String ep1() { return "Hello from EndPoint1"; }
+  @RequestMapping("/endPoint2")        public String ep2() { return "Hello from EndPoint2"; }
+  @RequestMapping("/endPoint3")        public String ep3() { return "Hello from EndPoint3"; }
+  @RequestMapping("/cars/endPoint4")   public String ep4() { return "Hello from EndPoint4"; }
+  @RequestMapping("/planes/endPoint5") public String ep5() { return "Hello from EndPoint5"; }
 }
 
