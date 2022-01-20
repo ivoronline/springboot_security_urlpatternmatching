123c7e9887f7456deef75a1e504758c7341f6913
ivoronline <ivoronline@gmail.com>
2022-01-20 22:16:12 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_urlpatternmatching/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_urlpatternmatching/config/WebSecurityConfig.java
@@ -1,32 +1,11 @@
 package com.ivoronline.springboot_security_urlpatternmatching.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  //=================================================================
-  // CONFIGURE (Auth...)
-  //=================================================================
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("myuser" ).password("myuserpassword" ).roles("USER" );
-    auth.inMemoryAuthentication().withUser("myadmin").password("myadminpassword").roles("ADMIN");
-  }
-
-  //=======================================================================
-  // PASSWORD ENCODER
-  //=======================================================================
-  @Bean
-  PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
-  }
 
   //=================================================================
   // CONFIGURE
@@ -36,23 +15,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     //SELECT SPECIFIC ENDPOINTS
     httpSecurity.authorizeRequests()
-      .antMatchers("/endPoint1")              .hasRole("USER")  //Select single  Endpoint
-      .antMatchers("/endPoint2", "/endPoint3").hasRole("USER"); //Select multiple Endpoints from the list
+      .antMatchers("/EndPoint1")              .hasRole("USER")  //Select single  Endpoint
+      .antMatchers("/EndPoint2", "/EndPoint3").hasRole("USER"); //Select multiple Endpoints from the list
 
     //SELECT ENDPOINTS - USING ASTERISK '*'
     httpSecurity.authorizeRequests()
-      .antMatchers("/*"                  ).hasRole("USER")  //Select Endpoints-this level
-      .antMatchers("/cars/*"             ).hasRole("USER")  //Select Endpoints-this level
-      .antMatchers("/cars/end*"          ).hasRole("USER")  //Select Endpoints-this level-start with /end
-      .antMatchers("/cars/*", "/bikes*"  ).hasRole("USER"); //Select Endpoints that match listed URL Patterns
+      .antMatchers("/*"                  ).hasRole("USER")      //Select Endpoints-this level
+      .antMatchers("/cars/*"             ).hasRole("USER")      //Select Endpoints-this level
+      .antMatchers("/cars/end*"          ).hasRole("USER")      //Select Endpoints-this level-start with /end
+      .antMatchers("/cars/*", "/bikes*"  ).hasRole("USER");     //Select Endpoints that match listed URL Patterns
 
     //SELECT ENDPOINTS - USING DOUBLE ASTERISK '**'
     httpSecurity.authorizeRequests()
-      .antMatchers("/**"                 ).hasRole("USER")  //Select Endpoints-any level
-      .antMatchers("/end**"              ).hasRole("USER")  //Select Endpoints-any level-start with /end
-      .antMatchers("/cars/**"            ).hasRole("USER")  //Select Endpoints-any level-start with /cars/
-      .antMatchers("/cars/end**"         ).hasRole("USER")  //Select Endpoints-any level-start with this
-      .antMatchers("/cars/*", "/bikes/**").hasRole("USER"); //Select Endpoints that match listed URL Patterns
+      .antMatchers("/**"                 ).hasRole("USER")      //Select Endpoints-any level
+      .antMatchers("/end**"              ).hasRole("USER")      //Select Endpoints-any level-start with /end
+      .antMatchers("/cars/**"            ).hasRole("USER")      //Select Endpoints-any level-start with /cars/
+      .antMatchers("/cars/end**"         ).hasRole("USER")      //Select Endpoints-any level-start with this
+      .antMatchers("/cars/*", "/bikes/**").hasRole("USER");     //Select Endpoints that match listed URL Patterns
 
     //AUTHENTICATE WITH DEFAULT LOGIN FORM
     httpSecurity.formLogin(); //Without this there is no Login Form to Authenticate
