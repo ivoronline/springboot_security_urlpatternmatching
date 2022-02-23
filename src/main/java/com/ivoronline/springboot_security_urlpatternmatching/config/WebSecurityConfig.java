98c24b9222a22249b33ccc53ebe47441e388be92
ivoronline <ivoronline@gmail.com>
2022-02-23 21:17:52 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_urlpatternmatching/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_urlpatternmatching/config/WebSecurityConfig.java
@@ -15,29 +15,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     //SELECT SPECIFIC ENDPOINTS
     httpSecurity.authorizeRequests()
-      .antMatchers("/EndPoint1")             .hasRole("USER")   //Select single   Endpoint
-      .antMatchers("/EndPoint2","/EndPoint3").hasRole("USER")   //Select multiple Endpoints from the list
-      .antMatchers("/EndPoint4")             .hasRole("ADMIN");
+      .antMatchers("/EndPoint2")             .hasRole("USER")   //Select single   Endpoint
+      .antMatchers("/EndPoint3","/EndPoint4").hasRole("USER")   //Select multiple Endpoints from the list
+      .antMatchers("/EndPoint5")             .hasRole("ADMIN");
 
-    //SELECT ENDPOINTS AT ASTERISK LEVEL
+    //SELECT ENDPOINTS USING URL PATTERNS
     httpSecurity.authorizeRequests()
-      .antMatchers("/*"              ).hasRole("USER")     //1st level Endpoint
-      .antMatchers("/end*"           ).hasRole("USER")     //1st level Endpoint that starts with /end
-      .antMatchers("/bikes/*"        ).hasRole("USER")     //2nd level Endpoint that starts with /bikes/
-      .antMatchers("/bikes/end*"     ).hasRole("USER")     //2nd level Endpoint that starts with /bikes/end
-      .antMatchers("/end*","/bikes/*").hasRole("USER");    //Endpoint that matches listed URL Patterns
-
-    //SELECT ENDPOINTS AT ASTERISK LEVEL AND BELOW
-    httpSecurity.authorizeRequests()
-      .antMatchers("/**"               ).hasRole("USER")   //Any level Endpoint
-      .antMatchers("/end**"            ).hasRole("USER")   //Any level Endpoint that starts with /end
-      .antMatchers("/cars/**"          ).hasRole("USER")   //Any level Endpoint that starts with /cars/
-      .antMatchers("/cars/end**"       ).hasRole("USER")   //Any level Endpoint that starts with /cars/end
-      .antMatchers("/end**", "/cars/**").hasRole("USER");  //Endpoint that matches listed URL Patterns
+      .antMatchers("/bikes/*").hasRole("USER")    //Second level:  /bikes/EndPoint6
+      .antMatchers("/cars/**").hasRole("USER");   //Any sub-level: /cars/bmw/EndPoint7
 
     //AUTHENTICATE WITH DEFAULT LOGIN FORM
-    httpSecurity.formLogin(); //Without this there is no Login Form to Authenticate
+    httpSecurity.formLogin();                     //Without this there is no Login Form to Authenticate
 
   }
 
 }
+
+
--- src/main/java/com/ivoronline/springboot_security_urlpatternmatching/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_urlpatternmatching/controllers/MyController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-  //ADDED ROLE USING EXPLICIT NAMES
-  @RequestMapping("/EndPoint1")          public String ep1() { return "Hello from EndPoint1"; }  //USER
+  //NO ROLES ADDED
+  @RequestMapping("/EndPoint1")          public String ep1() { return "Hello from EndPoint1"; }  //ANONYMOUS ACCESS
+
+  //ROLE ADDED USING EXPLICIT NAMES
   @RequestMapping("/EndPoint2")          public String ep2() { return "Hello from EndPoint2"; }  //USER
   @RequestMapping("/EndPoint3")          public String ep3() { return "Hello from EndPoint3"; }  //USER
-  @RequestMapping("/EndPoint4")          public String ep4() { return "Hello from EndPoint4"; }  //ADMIN
+  @RequestMapping("/EndPoint4")          public String ep4() { return "Hello from EndPoint4"; }  //USER
+  @RequestMapping("/EndPoint5")          public String ep5() { return "Hello from EndPoint5"; }  //ADMIN
 
-  //ADDED ROLE USING URL PATTERNS
-  @RequestMapping("/bikes/EndPoint5")    public String ep5() { return "Hello from EndPoint5"; }  //USER
-  @RequestMapping("/cars/bmw/EndPoint6") public String ep6() { return "Hello from EndPoint6"; }  //USER
+  //ROLE ADDED USING URL PATTERNS
+  @RequestMapping("/bikes/EndPoint6")    public String ep6() { return "Hello from EndPoint6"; }  //USER
+  @RequestMapping("/cars/bmw/EndPoint7") public String ep7() { return "Hello from EndPoint7"; }  //USER
 
 }
 
